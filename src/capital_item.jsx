875bef4b95e35ff1f90b83c7b9d2099d971084b5
liuhao <liuyihaotx@163.com>
2018-12-19 15:41:58 +0800

    可以使用固定数据展示资金明细列表了

--- src/capital_item.jsx
+++ src/capital_item.jsx
@@ -1,32 +1,44 @@
 import React, { Component } from "react";
-import {Row, Col} from 'antd'
+import {Row, Col, Divider} from 'antd'
 
 class CapitalItem extends Component {
-    // constructor(props) {
-    //     super(props)
-    // }
+    constructor(props) {
+        super(props)
+    }
 
     render() {
-        const _style = {
-            background: "green",
-            width: "900px",
-            hight: "400px"
-        };
-        const _style2 = {
-          background: "red"
-        };
-        return (
-          <div style={_style}>
-            <Row style={_style}>
-              <Col xs={21} sm={21} lg={21} style={_style2}>
-                <div><p>+166.66</p></div>
-              </Col>
-              <Col xs={3} sm={3} lg={3} style={_style2}>
-                <div><p>+188.88</p></div>
-              </Col>
-            </Row>
-          </div>
-        );
+      const item = this.props.data;
+      
+      const div_style = {
+        marginLeft: "10px",
+        marginRight: "10px",
+        marginTop: "5px"
+      }
+      const t_style = {
+        marginBottom: "1px"
+      }
+      const b_style = {
+        marginTop: "1px"
+      }
+      const divider_style = {
+        marginTop: "2px",
+        marginBottom: "0",
+        height: "1px"
+      };
+      return (
+        <div style={div_style}>
+          <Row type="flex" align="middle">
+            <Col xs={14} sm={14} lg={14}>
+                  <div><p align="left" style={t_style}>{item.desc}</p></div>
+                  <div><p align="left" style={b_style}>{item.time}</p></div>
+            </Col>
+            <Col xs={10} sm={10} lg={10}>
+              <div><p align="right">{item.amount}</p></div>
+            </Col>
+            <Divider style={divider_style}></Divider>
+          </Row>
+        </div>
+      );
     }
 }
 
---
+++ src/capital_list.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import CapitalItem from "./capital_item";
+import moment from "moment"
+
+class CapitalList extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const data = this.props.data.map(item => {
+            let new_item = {
+                amount: item.amount,
+                time: moment(new Date(item.time)).format("YYYY-MM-DD HH:mm:ss"),
+                order: item.order
+            }
+            switch(item.type) {
+                case "gain":
+                    new_item["amount"] = "+" + item.amount
+                    new_item["desc"] = "收益"
+                    break 
+                case "buy":
+                    new_item["amount"] = "-" + item.amount
+                    new_item["desc"] = "支出"
+                    break
+                case "recharge":
+                    new_item["amount"] = "+" + item.amount
+                    new_item["desc"] = "充值"
+                    break 
+                case "withdraw":
+                    new_item["amount"] = "-" + item.amount
+                    new_item["desc"] = "提现"
+                    break     
+                case "follower":
+                    new_item["amount"] = "+" + item.amount
+                    new_item["desc"] = "领投费"
+                    break 
+                case "followed":
+                    new_item["amount"] = "-" + item.amount
+                    new_item["desc"] = "跟投费"
+                    break      
+                case "early_return":
+                    new_item["amount"] = "+" + item.amount
+                    new_item["desc"] = "提前返还"
+                    break 
+                case "buy_fee":
+                    new_item["amount"] = "-" + item.amount
+                    new_item["desc"] = "服务费"
+                    break                             
+                default:
+                    break
+            }
+            return new_item
+        })
+        return (
+            data.map(item => {
+                return (
+                    <React.Fragment key={item.order}>
+                        <CapitalItem data={item}></CapitalItem>
+                    </React.Fragment>
+                )
+            })
+        )
+    }
+}
+
+export default CapitalList
